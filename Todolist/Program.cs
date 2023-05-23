8279c94675e17c9938352f0549dadea3e774d069
Zoey Baker <zoeybaker625@gmail.com>
2023-05-23 09:52:42 -0500

    initial commit

---
+++ Todolist/Program.cs
@@ -0,0 +1,15 @@
+﻿List<string> tasks = new List<string> { "Read crypto article", "Make vaporwave music", "Code Project" };
+
+string firstTask = tasks[0];
+string secondTask = tasks[1];
+string thirdTask = tasks[2];
+
+
+foreach (string task in tasks)
+
+{
+    Console.WriteLine(task);
+}
+
+
+
